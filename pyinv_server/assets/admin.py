e3f6a48d31fc0159bab24361360e2470c70ca1aa
Dan Trickey <git@trickey.io>
2022-05-14 19:03:29 +0100

    Add manufactuer model to the admin panel

--- pyinv_server/assets/admin.py
+++ pyinv_server/assets/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Manufacturer
+
+
+class ManufacturerAdmin(admin.ModelAdmin):
+    list_display = ["name", "slug"]
+    search_fields = ["name"]
+
+
+admin.site.register(Manufacturer, ManufacturerAdmin)
