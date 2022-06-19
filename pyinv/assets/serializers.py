85e5100dc5a48b38938d8fe2b71570a2352b2f7f
Dan Trickey <git@trickey.io>
2022-06-19 18:37:46 +0100

    Add manufacturer_slug to AssetModel serializer

--- pyinv/assets/serializers.py
+++ pyinv/assets/serializers.py
@@ -53,6 +53,7 @@ class AssetModelSerializer(serializers.ModelSerializer):
     """Serializer for AssetModel objects."""
     slug = serializers.CharField(allow_null=True, required=False)
     manufacturer = ManufacturerLinkSerializer(read_only=True)
+    manufacturer_slug = serializers.SlugRelatedField(slug_field="slug", source="manufacturer", queryset=Manufacturer.objects.all())
     asset_count = serializers.IntegerField(read_only=True, source="asset_set.count")
 
     class Meta:
@@ -61,6 +62,7 @@ class AssetModelSerializer(serializers.ModelSerializer):
             'name',
             'slug',
             'manufacturer',
+            'manufacturer_slug',
             'is_container',
             'asset_count',
             'created_at',
