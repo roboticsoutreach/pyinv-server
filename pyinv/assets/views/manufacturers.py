6f6a60c33f8bd422471a6267e28f94dda20dfef6
Dan Trickey <git@trickey.io>
2022-06-19 14:22:18 +0100

    Add create, update and delete methods to manufacturers

--- pyinv/assets/views/manufacturers.py
+++ pyinv/assets/views/manufacturers.py
@@ -1,10 +1,11 @@
+from django.db.models import ProtectedError
 from rest_framework import filters, viewsets
 
 from assets.models import Manufacturer
 from assets.serializers import ManufacturerSerializer
+from pyinv.api_exceptions import UnableToDelete
 
-
-class ManufacturerViewSet(viewsets.ReadOnlyModelViewSet):
+class ManufacturerViewSet(viewsets.ModelViewSet):
     """Fetch information about manufacturers."""
 
     queryset = Manufacturer.objects.all()
@@ -13,3 +14,9 @@ class ManufacturerViewSet(viewsets.ReadOnlyModelViewSet):
     filter_backends = [filters.SearchFilter, filters.OrderingFilter]
     ordering_fields = ["name", "slug", 'created_at', 'updated_at']
     search_fields = ["name", "slug"]
+
+    def perform_destroy(self, instance):
+        try:
+            return super(ManufacturerViewSet, self).perform_destroy(instance)
+        except ProtectedError:
+            raise UnableToDelete()
---
+++ pyinv/pyinv/api_exceptions.py
@@ -0,0 +1,8 @@
+from rest_framework import status
+
+from rest_framework.exceptions import APIException
+
+class UnableToDelete(APIException):
+    status_code = status.HTTP_400_BAD_REQUEST
+    default_detail = 'Unable to delete object because it is referenced by other objects.'
+    default_code = 'protected_error'
